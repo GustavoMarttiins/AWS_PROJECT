3a08c9f5767ab663670570a4671e213a1810302d
Martins <gustavo_marttiins@hotmail.com>
2024-07-02 12:44:10 -0300

    novo projeto

---
+++ src/main/java/br/com/projeto/projeto_aws/ProjetoAwsApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.projeto_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAwsApplication.class, args);
+	}
+
+}
